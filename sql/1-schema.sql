906bebd369035d01efeacbdc842b92868c0ca8ea
Yuhi-Sato <yuhi120101@gmail.com>
2024-12-24 06:27:16 +0900

    index

--- sql/1-schema.sql
+++ sql/1-schema.sql
@@ -47,6 +47,7 @@ CREATE TABLE chair_locations
   PRIMARY KEY (id)
 )
   COMMENT = '椅子の現在位置情報テーブル';
+ALTER TABLE chair_locations ADD INDEX idx_chair_id_created_at_desc (chair_id, created_at DESC);
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE users
@@ -106,6 +107,7 @@ CREATE TABLE ride_statuses
   PRIMARY KEY (id)
 )
   COMMENT = 'ライドステータスの変更履歴テーブル';
+ALTER TABLE ride_statuses ADD INDEX idx_ride_id (ride_id);
 
 DROP TABLE IF EXISTS owners;
 CREATE TABLE owners
