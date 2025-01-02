74c31835fa7577da3b113c358ebd6b4bcb160498
Yuhi-Sato <yuhi120101@gmail.com>
2025-01-02 11:17:10 +0900

    最新のride_statusとjoin

--- go/app_handlers.go
+++ go/app_handlers.go
@@ -678,108 +678,6 @@ type appGetNotificationResponseChairStats struct {
 	TotalEvaluationAvg float64 `json:"total_evaluation_avg"`
 }
 
-// func appGetNotification(w http.ResponseWriter, r *http.Request) {
-// 	ctx := r.Context()
-// 	user := ctx.Value("user").(*User)
-
-// 	tx, err := db.Beginx()
-// 	if err != nil {
-// 		writeError(w, http.StatusInternalServerError, err)
-// 		return
-// 	}
-// 	defer tx.Rollback()
-
-// 	ride := &Ride{}
-// 	if err := tx.GetContext(ctx, ride, `SELECT * FROM rides WHERE user_id = ? ORDER BY created_at DESC LIMIT 1`, user.ID); err != nil {
-// 		if errors.Is(err, sql.ErrNoRows) {
-// 			writeJSON(w, http.StatusOK, &appGetNotificationResponse{
-// 				RetryAfterMs: 30,
-// 			})
-// 			return
-// 		}
-// 		writeError(w, http.StatusInternalServerError, err)
-// 		return
-// 	}
-
-// 	yetSentRideStatus := RideStatus{}
-// 	status := ""
-// 	if err := tx.GetContext(ctx, &yetSentRideStatus, `SELECT * FROM ride_statuses WHERE ride_id = ? AND app_sent_at IS NULL ORDER BY created_at ASC LIMIT 1`, ride.ID); err != nil {
-// 		if errors.Is(err, sql.ErrNoRows) {
-// 			status, err = getLatestRideStatus(ctx, tx, ride.ID)
-// 			if err != nil {
-// 				writeError(w, http.StatusInternalServerError, err)
-// 				return
-// 			}
-// 		} else {
-// 			writeError(w, http.StatusInternalServerError, err)
-// 			return
-// 		}
-// 	} else {
-// 		status = yetSentRideStatus.Status
-// 	}
-
-// 	fare, err := calculateDiscountedFare(ctx, tx, user.ID, ride, ride.PickupLatitude, ride.PickupLongitude, ride.DestinationLatitude, ride.DestinationLongitude)
-// 	if err != nil {
-// 		writeError(w, http.StatusInternalServerError, err)
-// 		return
-// 	}
-
-// 	response := &appGetNotificationResponse{
-// 		Data: &appGetNotificationResponseData{
-// 			RideID: ride.ID,
-// 			PickupCoordinate: Coordinate{
-// 				Latitude:  ride.PickupLatitude,
-// 				Longitude: ride.PickupLongitude,
-// 			},
-// 			DestinationCoordinate: Coordinate{
-// 				Latitude:  ride.DestinationLatitude,
-// 				Longitude: ride.DestinationLongitude,
-// 			},
-// 			Fare:      fare,
-// 			Status:    status,
-// 			CreatedAt: ride.CreatedAt.UnixMilli(),
-// 			UpdateAt:  ride.UpdatedAt.UnixMilli(),
-// 		},
-// 		RetryAfterMs: 30,
-// 	}
-
-// 	if ride.ChairID.Valid {
-// 		chair := &Chair{}
-// 		if err := tx.GetContext(ctx, chair, `SELECT * FROM chairs WHERE id = ?`, ride.ChairID); err != nil {
-// 			writeError(w, http.StatusInternalServerError, err)
-// 			return
-// 		}
-
-// 		stats, err := getChairStats(ctx, tx, chair.ID)
-// 		if err != nil {
-// 			writeError(w, http.StatusInternalServerError, err)
-// 			return
-// 		}
-
-// 		response.Data.Chair = &appGetNotificationResponseChair{
-// 			ID:    chair.ID,
-// 			Name:  chair.Name,
-// 			Model: chair.Model,
-// 			Stats: stats,
-// 		}
-// 	}
-
-// 	if yetSentRideStatus.ID != "" {
-// 		_, err := tx.ExecContext(ctx, `UPDATE ride_statuses SET app_sent_at = CURRENT_TIMESTAMP(6) WHERE id = ?`, yetSentRideStatus.ID)
-// 		if err != nil {
-// 			writeError(w, http.StatusInternalServerError, err)
-// 			return
-// 		}
-// 	}
-
-// 	if err := tx.Commit(); err != nil {
-// 		writeError(w, http.StatusInternalServerError, err)
-// 		return
-// 	}
-
-// 	writeJSON(w, http.StatusOK, response)
-// }
-
 func appGetNotificationWithSSE(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	user := ctx.Value("user").(*User)
@@ -1125,7 +1023,7 @@ func appGetNearbyChairs(w http.ResponseWriter, r *http.Request) {
 	err = tx.SelectContext(
 		ctx,
 		&chairs,
-		`SELECT * FROM chairs`,
+		`SELECT * FROM chairs WHERE is_active = 1`,
 	)
 	if err != nil {
 		writeError(w, http.StatusInternalServerError, err)
@@ -1134,12 +1032,25 @@ func appGetNearbyChairs(w http.ResponseWriter, r *http.Request) {
 
 	nearbyChairs := []appGetNearbyChairsResponseChair{}
 	for _, chair := range chairs {
-		if !chair.IsActive {
-			continue
-		}
+		// if !chair.IsActive {
+		// 	continue
+		// }
 
 		rides := []*Ride{}
-		if err := tx.SelectContext(ctx, &rides, `SELECT * FROM rides WHERE chair_id = ? ORDER BY created_at DESC`, chair.ID); err != nil {
+		query := `
+		SELECT r.* FROM rides r
+		LEFT JOIN (
+			SELECT *
+			FROM (
+				SELECT ride_statuses.*, ROW_NUMBER() over (PARTITION BY ride_id ORDER BY created_at DESC ) AS latest
+				FROM ride_statuses
+				) as tmp
+			WHERE latest = 1
+		) lrs
+		ON lrs.ride_id = r.id
+		WHERE r.chair_id = ? AND status = "COMPLETED" ORDER BY r.created_at DESC
+		`
+		if err := tx.SelectContext(ctx, &rides, query, chair.ID); err != nil {
 			writeError(w, http.StatusInternalServerError, err)
 			return
 		}
@@ -1190,16 +1101,16 @@ func appGetNearbyChairs(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	retrievedAt := &time.Time{}
-	err = tx.GetContext(
-		ctx,
-		retrievedAt,
-		`SELECT CURRENT_TIMESTAMP(6)`,
-	)
-	if err != nil {
-		writeError(w, http.StatusInternalServerError, err)
-		return
-	}
+	retrievedAt := time.Now()
+	// err = tx.GetContext(
+	// 	ctx,
+	// 	retrievedAt,
+	// 	`SELECT CURRENT_TIMESTAMP(6)`,
+	// )
+	// if err != nil {
+	// 	writeError(w, http.StatusInternalServerError, err)
+	// 	return
+	// }
 
 	writeJSON(w, http.StatusOK, &appGetNearbyChairsResponse{
 		Chairs:      nearbyChairs,
