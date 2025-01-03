891a1d302a4ce10492fd0e618f6942b1068774d5
Yuhi-Sato <yuhi120101@gmail.com>
2025-01-03 21:32:32 +0900

    通知時間

--- go/main.go
+++ go/main.go
@@ -111,7 +111,7 @@ func chairTotalDistanceProcess(ctx context.Context) {
 		select {
 		case chairTotalDistance := <-chairTotalDistanceCh:
 			chairTotalDistances = append(chairTotalDistances, chairTotalDistance)
-		case <-time.After(500 * time.Millisecond):
+		case <-time.After(2 * time.Second):
 			if len(chairTotalDistances) == 0 {
 				continue
 			}
